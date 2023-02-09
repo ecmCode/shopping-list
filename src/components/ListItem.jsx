aa1e4a283a489ca562323f3ab63cbe50758dbb97
Lee <elcheveremochilero@gmail.com>
2023-02-09 15:39:13 -0600

    add icons

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,10 +1,20 @@
 import style from "./ListItem.module.css"
-
+import { ImCheckboxUnchecked, ImCheckboxChecked } from 'react-icons/im';
+import { FaTrash } from 'react-icons/fa'
 
 const ListItem = ({ index, data }) => {
     return (
         <div className={style.listItem}>
-            {index}. {data.item} {data.price}
+            <span className={style.data}>
+                <span className={style.checkbox}>
+                    <ImCheckboxChecked/>
+                    <ImCheckboxUnchecked/>
+                </span>
+                {index}. {data.item} {data.price}
+            </span>
+            <span>
+                <FaTrash/>
+            </span>
         </div>
     );
 }
