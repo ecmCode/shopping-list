e043cf77fa3fafced0c20c7dcc703b075f918839
Lee <elcheveremochilero@gmail.com>
2023-02-13 10:25:04 -0600

    add footer with calculating components

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import db from "../db/db.json";
 import ListItem from "./ListItem";
 import style from "./List.module.css"
+import Footer from "./Footer";
 
 const List = () => {
 
@@ -25,9 +26,8 @@ const List = () => {
                         )}
                     )
                 }
-                
             </div>
-            
+            <Footer data={data}/>
         </div>
      );
 }
