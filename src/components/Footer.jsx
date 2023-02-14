24664f58133b52ca08ab87b42c0d695f36de4144
Lee <elcheveremochilero@gmail.com>
2023-02-14 12:38:00 -0600

    use reduce method

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
 import style from "./Footer.module.css"
 
 const Footer = ({data}) => {
-    let total = 0
-    data.forEach(i => total += i.price)
     return ( 
         <div className={style.footer}>
             <div>
                 Items: {data.length}
             </div>
             <div>
-                Total: {total}
+                Total: {data.reduce((total,item) => {
+                    return total + item.price
+                },0)}
             </div>
         </div>
-     );
+    );
 }
  
 export default Footer;
