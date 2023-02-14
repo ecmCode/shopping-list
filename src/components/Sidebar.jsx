20a7a28418ba1feb8f22c0812426c5c61f2aba03
Lee <elcheveremochilero@gmail.com>
2023-02-13 19:29:16 -0600

    add sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import style from "./Sidebar.module.css"
+
+const Sidebar = () => {
+    return ( 
+        <div className={style.sidebar}>
+            
+        </div>
+     );
+}
+ 
+export default Sidebar;
