873d22ddf9f3737f07ef583b2d2ab0071d32ba74
Lee <elcheveremochilero@gmail.com>
2023-02-15 09:58:09 -0600

    add inputs and btns to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import style from "./Sidebar.module.css"
 
-const Sidebar = () => {
+const Sidebar = ({data, setData}) => {
+    const [item,setItem] = useState(null)
+    const [price,setPrice] = useState(null)
     return ( 
         <div className={style.sidebar}>
-            
+            <div className={style.input}>
+                <span>
+                    <label htmlFor="item">Item</label>
+                    <input 
+                    id="item" 
+                    name="item" 
+                    type="text" 
+                    value={item}/>
+                </span>
+                <span>
+                    <label htmlFor="price">Price</label>
+                    <input 
+                    id="price" 
+                    name="price" 
+                    type="text" 
+                    value={price}/>
+                </span>        
+            </div>
+
+            <div className={style.btns}>
+                <button className={style.addbtn}>Add</button>
+                <button className={style.clearbtn}>Clear</button>
+            </div>
+
         </div>
      );
 }
